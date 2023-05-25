d96ed40546c8b16de308e6086f88b2002f1efa3d
AnhMH <anh.mh@moziasoft.com>
2023-05-25 15:33:45 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(GenreSeeder::class);
-        \App\Models\User::factory(20)->create();
+        // $this->call(GenreSeeder::class);
+        // \App\Models\User::factory(20)->create();
         \App\Models\User::factory()->create([
             'username' => 'admin',
             'name' => 'Admin User',
